9504db8c4b7653695b037da8b79ee8a85e7d877e
dcharua <danielcharua@hotmail.com>
2018-10-10 15:30:13 -0500

    clase 09/10/18

---
+++ Actividades/A12.2.Selenium1/ejercicio.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+
+browser.get('http://blazedemo.com/')
+# assert 'Yahoo' in browser.title
+
+select = browser.find_element_by_name('fromPort')
+options = select.find_elements_by_tag_name('option')
+
+for option in options:
+    if option.get_attribute('value') == "Mexico City":
+        option.click()
+
+select = browser.find_element_by_name('toPort')
+options = select.find_elements_by_tag_name('option')
+
+for option in options:
+    if option.get_attribute('value') == "Paris":
+        option.click()
+
+browser.find_element_by_xpath('/html/body/div[3]/form/div/input').click()
+
+table = browser.find_element_by_xpath('/html/body/div[2]/table/tbody')
+prices = table.find_elements_by_tag_name('tr')
+
+lower = None
+lowerIndex = None
+for i,price in enumerate(prices):
+    rowPrice = browser.find_element_by_xpath('/html/body/div[2]/table/tbody/tr['+str(i+1)+']/td[6]').text
+    if lower == None or rowPrice < lower:
+        lower = rowPrice
+        lowerIndex = i+1
+
+browser.find_element_by_xpath('/html/body/div[2]/table/tbody/tr['+str(lowerIndex)+']/td[1]/input').click()
+
+
+browser.find_element_by_name('inputName').send_keys("Daniel Charua")
+browser.find_element_by_name('address').send_keys('Av de los Bosques')
+browser.find_element_by_name('city').send_keys('Mexico City')
+browser.find_element_by_name('state').send_keys('Mexico')
+browser.find_element_by_name('zipCode').send_keys('52780')
+browser.find_element_by_name('creditCardNumber').send_keys('4248-5215-2353-4535')
+browser.find_element_by_name('creditCardMonth').send_keys(Keys.BACK_SPACE+Keys.BACK_SPACE+'10')
+browser.find_element_by_name('creditCardYear').send_keys(Keys.BACK_SPACE+Keys.BACK_SPACE+Keys.BACK_SPACE+Keys.BACK_SPACE+'2022')
+browser.find_element_by_name('nameOnCard').send_keys('Daniel Charua')
+
+browser.find_element_by_xpath('/html/body/div[2]/form/div[11]/div/input').click()
